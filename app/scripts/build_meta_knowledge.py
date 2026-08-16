1aaab4e30dca0433c2c9d7a78865711772a366a3
Azusa <m3171470760@gmail.com>
2026-08-16 15:20:18 +0800

    feat(scripts):add build script

---
+++ app/scripts/build_meta_knowledge.py
@@ -0,0 +1,13 @@
+from app.core.log import logger
+
+
+def build():
+    """
+    构建元知识库函数
+    用于执行构建元知识库相关的业务逻辑，例如加载配置、初始化数据等
+    :return:
+    """
+    logger.info("Building meta knowledge...")
+
+if __name__ == "__main__":
+    build()
