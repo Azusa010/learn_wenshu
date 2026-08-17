641a946214aaa1c127e74bceccdb933e51643e55
Azusa <m3171470760@gmail.com>
2026-08-17 15:49:53 +0800

    feat(agent):add primary key and foreign key

--- app/agent/nodes/merge_retrieved_info.py
+++ app/agent/nodes/merge_retrieved_info.py
@@ -5,6 +5,7 @@ from langgraph.runtime import Runtime
 from app.agent.context import DataAgentContext
 from app.agent.state import DataAgentState
 from app.models.mysql.column_info_mysql import ColumnInfoMySQL
+from app.models.mysql.table_info_mysql import TableInfoMySQL
 from app.models.qdrant.column_info_qdrant import ColumnInfoQdrant
 from app.models.qdrant.metric_info_qdrant import MetricInfoQdrant
 from app.models.value_info_es.value_info_es import ValueInfoEs
@@ -59,3 +60,20 @@ async def merge_retrieved_info(state: DataAgentState, runtime: Runtime[DataAgent
 
         if column_value not in retrieved_column_maps[column_id]["examples"]:
             retrieved_column_maps[column_id]["examples"].append(column_value)
+
+    # 3.划分字段以表为单位
+    retrieved_table_maps: dict[str, list[ColumnInfoQdrant]] = {}
+    for column_info in retrieved_column_maps.values():
+        table_id = column_info["table_id"]
+        if table_id not in retrieved_table_maps:
+            retrieved_table_maps[table_id] = []
+
+    # 4.遍历表字段结构数据，增加主外键数据
+    for table_id, column_list in retrieved_table_maps.items():
+        column_infos: list[ColumnInfoMySQL] = await meta_mysql_repository.get_key_column_by_table_id(table_id)
+        # 判断
+        column_ids: list[str] = [columns["id"] for columns in column_list]
+        for column_info in column_infos:
+            column_id = column_info.id
+            if column_id not in column_ids:
+                column_list.append(convert_column_info_from_mysql_to_qdrant(column_info))
