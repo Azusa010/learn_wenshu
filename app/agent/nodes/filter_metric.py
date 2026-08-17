6a42e154c5750876385a7c2dba0cbab1248134fd
Azusa <m3171470760@gmail.com>
2026-08-17 19:37:48 +0800

    feat(agent):add filter_metric node

--- app/agent/nodes/filter_metric.py
+++ app/agent/nodes/filter_metric.py
@@ -1,19 +1,40 @@
 import asyncio
 
+import yaml
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
 
 from app.agent.context import DataAgentContext
+from app.agent.llm import llm
 from app.agent.state import DataAgentState
 from app.core.log import logger
+from app.prompt.prompt_loader import load_prompt
 
 
-async def filter_metric(state:DataAgentState,runtime:Runtime[DataAgentContext]):
+async def filter_metric(state: DataAgentState, runtime: Runtime[DataAgentContext]):
     await asyncio.sleep(1)
     # 获取流对象
     writer = runtime.stream_writer
     writer({"state": "过滤指标"})
     try:
-        pass
+        query = state["query"]
+        metric_infos = state["metric_infos"]
+        # 调用模型,返回与业务相关的指标信息
+        prompt = PromptTemplate(template=load_prompt("filter_metric_info"), input_variables=["query", "metric_infos"])
+        output_parser = JsonOutputParser()
+        chain = prompt | llm | output_parser
+        result = await chain.ainvoke(
+            {"query": query, "metric_infos": yaml.dump(metric_infos, allow_unicode=True, sort_keys=False)})
+
+        # 遍历
+        for metric_info in metric_infos[:]:
+            metric_name = metric_info["name"]
+            if metric_name not in result:
+                metric_infos.remove(metric_info)
+        logger.info(f"过滤指标成功:{metric_infos}")
+        return {"metric_infos": metric_infos}
+
     except Exception as e:
-        logger.error(e)
+        logger.error(f"过滤指标异常:{str(e)}")
         raise
