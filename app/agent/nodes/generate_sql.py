7ce5a64c2e6ff2f5cea7690bfb49351d7e2fcb25
Azusa <m3171470760@gmail.com>
2026-08-17 20:10:20 +0800

    feat(agent):add generate_sql node

--- app/agent/nodes/generate_sql.py
+++ app/agent/nodes/generate_sql.py
@@ -1,9 +1,15 @@
 import asyncio
 
+import yaml
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
 
 from app.agent.context import DataAgentContext
+from app.agent.llm import llm
 from app.agent.state import DataAgentState
+from app.core.log import logger
+from app.prompt.prompt_loader import load_prompt
 
 
 async def generate_sql(state:DataAgentState,runtime:Runtime[DataAgentContext]):
@@ -11,3 +17,26 @@ async def generate_sql(state:DataAgentState,runtime:Runtime[DataAgentContext]):
     # 获取流对象
     writer = runtime.stream_writer
     writer({"state": "生成sql"})
+    try:
+        query = state["query"]
+        table_infos = state["table_infos"]
+        metric_infos = state["metric_infos"]
+        date_info = state["date_info"]
+        db_info = state["db_info"]
+
+        prompt = PromptTemplate(template=load_prompt("generate_sql"),input_variables=["query","table_infos","metric_infos","date_info","db_info"])
+        output_parser = StrOutputParser()
+        chain = prompt | llm | output_parser
+        sql = await chain.ainvoke(
+            {
+                "query": query,
+                "table_infos": yaml.dump(table_infos,allow_unicode=True,sort_keys=False),
+                "metric_infos": yaml.dump(metric_infos,allow_unicode=True,sort_keys=False),
+                "date_info": yaml.dump(date_info,allow_unicode=True,sort_keys=False),
+                "db_info": yaml.dump(db_info,allow_unicode=True,sort_keys=False),
+            }
+        )
+        logger.info(sql)
+    except Exception as e:
+        logger.error(e)
+        raise
