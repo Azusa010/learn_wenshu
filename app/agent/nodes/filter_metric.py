48ec7cdb7795d4efd5fb00dc7a346c072fdf28f6
Azusa <m3171470760@gmail.com>
2026-08-17 19:30:57 +0800

    feat(agent):add filter_table node

--- app/agent/nodes/filter_metric.py
+++ app/agent/nodes/filter_metric.py
@@ -4,6 +4,7 @@ from langgraph.runtime import Runtime
 
 from app.agent.context import DataAgentContext
 from app.agent.state import DataAgentState
+from app.core.log import logger
 
 
 async def filter_metric(state:DataAgentState,runtime:Runtime[DataAgentContext]):
@@ -11,3 +12,8 @@ async def filter_metric(state:DataAgentState,runtime:Runtime[DataAgentContext]):
     # 获取流对象
     writer = runtime.stream_writer
     writer({"state": "过滤指标"})
+    try:
+        pass
+    except Exception as e:
+        logger.error(e)
+        raise
--- app/agent/nodes/filter_table.py
+++ app/agent/nodes/filter_table.py
@@ -1,13 +1,47 @@
 import asyncio
 
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
 
 from app.agent.context import DataAgentContext
-from app.agent.state import DataAgentState
+from app.agent.llm import llm
+from app.agent.state import DataAgentState, TableInfoState
+from app.core.log import logger
+from app.prompt.prompt_loader import load_prompt
 
 
-async def filter_table(state:DataAgentState,runtime:Runtime[DataAgentContext]):
+async def filter_table(state: DataAgentState, runtime: Runtime[DataAgentContext]):
     await asyncio.sleep(1)
     # 获取流对象
     writer = runtime.stream_writer
     writer({"state": "过滤表信息"})
+    try:
+        query = state["query"]
+        table_infos: list[TableInfoState] = state["table_infos"]
+
+        # 1. 调用模型,返回与业务相关的表信息
+        # 1.1 定义提示词模板
+        prompt = PromptTemplate(template=load_prompt("filter_table_info"), input_variables=["query", "table_infos"])
+        output_parser = JsonOutputParser()
+        chain = prompt | llm | output_parser
+        result = await chain.ainvoke({"query": query, "table_infos": table_infos})
+
+        # 2. 循环遍历表信息，根据业务相关结果，剔除表信息
+        for table_info in table_infos:
+            table_name = table_info["name"]
+
+            if table_name not in result[:]:
+                table_infos.remove(table_info)
+            else:
+                # 模型处理后的结构
+                result_column = result[table_name]
+                for columns_info in table_info["columns"][:]:
+                    column_name = columns_info["name"]
+                    if column_name not in result_column:
+                        table_info["columns"].remove(columns_info)
+        logger.info(f"过滤表成功{table_infos}")
+        return {"table_infos": table_infos}
+    except Exception as e:
+        logger.error(f"过滤表异常f{str(e)}")
+        raise
