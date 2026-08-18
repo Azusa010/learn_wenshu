3b6d11c8455f818b3df9234c489ff65ea0697968
Azusa <m3171470760@gmail.com>
2026-08-18 09:00:03 +0800

    feat(agent):校正sql节点

--- app/agent/nodes/correct_sql.py
+++ app/agent/nodes/correct_sql.py
@@ -1,13 +1,49 @@
 import asyncio
 
+import yaml
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
 
 from app.agent.context import DataAgentContext
+from app.agent.llm import llm
 from app.agent.state import DataAgentState
+from app.core.log import logger
+from app.prompt.prompt_loader import load_prompt
 
 
-async def correct_sql(state:DataAgentState,runtime:Runtime[DataAgentContext]):
+async def correct_sql(state: DataAgentState, runtime: Runtime[DataAgentContext]):
     await asyncio.sleep(1)
     # 获取流对象
     writer = runtime.stream_writer
     writer({"state": "矫正sql语句"})
+    sql = state["sql"]
+    try:
+        error = state["error"]
+        query = state["query"]
+        table_infos = state["table_infos"]
+        metric_infos = state["metric_infos"]
+        date_info = state["date_info"]
+        db_info = state["db_info"]
+
+        prompt = PromptTemplate(template=load_prompt("generate_sql"),
+                                input_variables=["query", "table_infos", "metric_infos", "date_info", "db_info", "sql",
+                                                 "error"], )
+        output_parser = StrOutputParser()
+        chain = prompt | llm | output_parser
+        sql = await chain.ainvoke(
+            {
+                "query": query,
+                "table_infos": yaml.dump(table_infos, allow_unicode=True, sort_keys=False),
+                "metric_infos": yaml.dump(metric_infos, allow_unicode=True, sort_keys=False),
+                "date_info": yaml.dump(date_info, allow_unicode=True, sort_keys=False),
+                "db_info": yaml.dump(db_info, allow_unicode=True, sort_keys=False),
+                "sql": sql,
+                "error": error
+            }
+        )
+        logger.info(f"校正sql成功:{sql}")
+        return {"sql": sql}
+    except Exception as e:
+        logger.error(f"校正sql失败:{sql}")
+        raise
